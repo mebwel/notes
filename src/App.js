c1d0f46de216aed8a96f500bc164891923e07d14
zemeb <mebwed@gmail.com>
2022-04-05 09:39:21 +0300

    Initial repository

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './App.module.css'
+const App = () => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.typing}>
+                Welcome to notes project.
+            </div>
+        </div>
+    )
+}
+
+export default App
